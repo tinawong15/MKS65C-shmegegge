a7c95601b956b7096b5c9680f87c93e2876b310d
tina <tinaw425@gmail.com>
2018-12-08 00:20:38 -0500

    added work

---
+++ main.c
@@ -0,0 +1,51 @@
+# include <sys/shm.h>
+# include <sys/ipc.h>
+# include <sys/types.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+int main(int argc, char const *argv[]) {
+  /* Connects or creates a shared memory segment large enough to hold a 200 character string.
+  Displays the contents of the shared memory. */
+  int shmid;
+  char *data;
+  char string[256];
+  shmid = shmget(0, 200, 0644 | IPC_CREAT);
+  printf("Identifer for shared memory segment: %d\n", shmid);
+  data = shmat(shmid, (void *)0, 0);
+  printf("Pointer attached: %s\n", data);
+  /* If the segment was just created, instead show a message saying as much.*/
+  if(data) {
+    printf("Segment was just created!\n");
+  }
+  /* Ask the user if they want to change the data in the segment.
+  If yes, prompt the user to enter a new string and write that to the segment.
+  Ask the user if they want to delete the segment, if yes, then delete it. */
+  while(1) {
+    printf("Do you want to change the data in the segment? [y/n]\n");
+    fgets(string, 200, stdin);
+    string[strlen(string)-1] = 0;
+    if(strcmp(string, "y") == 0) {
+      printf("Enter a new string to the segment: \n");
+      fgets(string, 200, stdin);
+      string[strlen(string)-1] = 0;
+      strcpy(data, string);
+      printf("You entered: %s\n", data);
+    }
+    else if (strcmp(string, "n") == 0){
+      printf("Do you want to delete the segment? [y/n]\n");
+      fgets(string, 200, stdin);
+      string[strlen(string)-1] = 0;
+      if(strcmp(string, "y") == 0) {
+        break;
+      }
+    }
+    else {
+      printf("Try again!\n");
+    }
+  }
+  shmctl(shmid, IPC_RMID, NULL);
+  exit(0);
+  return 0;
+}
